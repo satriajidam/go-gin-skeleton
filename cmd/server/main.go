96e917def6f22cceef31b2aaa61182522190100d
Agastyo Satriaji Idam <agastyo@ruangguru.com>
2020-03-09 21:03:05 +0700

    Add config and basic logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,86 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/satriajidam/go-gin-skeleton/cmd/server/config"
+)
+
+func initLogger(config config.Config) {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	switch config.LogLevel {
+	case "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "trace":
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	}
+}
+
+func initDBConnection(config config.Config) (*gorm.DB, error) {
+	mode := config.DBMode
+	host := config.DBHost
+	port := config.DBPort
+	user := config.DBUser
+	pass := config.DBPass
+	name := config.DBName
+	params := config.DBParams
+	dsn := ""
+
+	switch mode {
+	case "mysql":
+		if params == "" {
+			params = "charset=utf8&parseTime=True&loc=Local"
+		}
+
+		dsn = fmt.Sprintf("%s:%s@%s:%s/%s?%s", user, pass, host, port, name, params)
+	case "postgres":
+		if params == "" {
+			params = "sslmode=disable"
+		}
+
+		dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?%s", user, pass, host, port, name, params)
+	case "mssql":
+		if params == "" {
+			params = "connection+timeout=0&dial+timeout=15&encrypt=false"
+		}
+
+		dsn = fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s&%s", user, pass, host, port, name, params)
+	}
+
+	db, err := gorm.Open(mode, dsn)
+	if err != nil {
+		return db, err
+	}
+
+	db.LogMode(config.GormEnableLog)
+
+	return db, nil
+}
+
 func main() {
+	config := config.Get()
+
+	initLogger(config)
+
+	db, err := initDBConnection(config)
+	if err != nil {
+		db.Close()
+		log.Print(err.Error())
+		panic(err)
+	}
 
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,10 @@ module github.com/satriajidam/go-gin-skeleton
 go 1.14
 
 require (
+	cloud.google.com/go v0.37.4 // indirect
 	github.com/gin-gonic/gin v1.5.0 // indirect
 	github.com/go-playground/validator/v10 v10.2.0 // indirect
-	github.com/jinzhu/gorm v1.9.11 // indirect
+	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/rs/zerolog v1.18.0 // indirect
 	github.com/stretchr/testify v1.5.1 // indirect
