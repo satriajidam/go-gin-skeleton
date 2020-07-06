8b74dd3587b3993a3c2a3266984c950224c4285b
Agastyo Satriaji Idam <agastyo@ruangguru.com>
2020-07-06 09:47:04 +0700

    Add new predefined route

--- pkg/server/http/predefined.go
+++ pkg/server/http/predefined.go
@@ -20,85 +20,12 @@ var predefinedRoutes = []predefinedRoute{
 	{
 		httpMethod:    http.MethodGet,
 		relateivePath: "/_/status/:code",
-		handlerFunc:
-		// Generates responses based on the given status code.
-		func(ctx *gin.Context) {
-			code, err := strconv.Atoi(ctx.Param("code"))
-			if err != nil {
-				ctx.String(http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
-				return
-			}
-
-			switch code {
-			case http.StatusContinue,
-				http.StatusSwitchingProtocols,
-				http.StatusProcessing,
-				http.StatusEarlyHints,
-				http.StatusOK,
-				http.StatusCreated,
-				http.StatusAccepted,
-				http.StatusNonAuthoritativeInfo,
-				http.StatusNoContent,
-				http.StatusResetContent,
-				http.StatusPartialContent,
-				http.StatusMultiStatus,
-				http.StatusAlreadyReported,
-				http.StatusIMUsed,
-				http.StatusMultipleChoices,
-				http.StatusMovedPermanently,
-				http.StatusFound,
-				http.StatusSeeOther,
-				http.StatusNotModified,
-				http.StatusUseProxy,
-				http.StatusTemporaryRedirect,
-				http.StatusPermanentRedirect,
-				http.StatusBadRequest,
-				http.StatusUnauthorized,
-				http.StatusPaymentRequired,
-				http.StatusForbidden,
-				http.StatusNotFound,
-				http.StatusMethodNotAllowed,
-				http.StatusNotAcceptable,
-				http.StatusProxyAuthRequired,
-				http.StatusRequestTimeout,
-				http.StatusConflict,
-				http.StatusGone,
-				http.StatusLengthRequired,
-				http.StatusPreconditionFailed,
-				http.StatusRequestEntityTooLarge,
-				http.StatusRequestURITooLong,
-				http.StatusUnsupportedMediaType,
-				http.StatusRequestedRangeNotSatisfiable,
-				http.StatusExpectationFailed,
-				http.StatusTeapot,
-				http.StatusMisdirectedRequest,
-				http.StatusUnprocessableEntity,
-				http.StatusLocked,
-				http.StatusFailedDependency,
-				http.StatusTooEarly,
-				http.StatusUpgradeRequired,
-				http.StatusPreconditionRequired,
-				http.StatusTooManyRequests,
-				http.StatusRequestHeaderFieldsTooLarge,
-				http.StatusUnavailableForLegalReasons,
-				http.StatusInternalServerError,
-				http.StatusNotImplemented,
-				http.StatusBadGateway,
-				http.StatusServiceUnavailable,
-				http.StatusGatewayTimeout,
-				http.StatusHTTPVersionNotSupported,
-				http.StatusVariantAlsoNegotiates,
-				http.StatusInsufficientStorage,
-				http.StatusLoopDetected,
-				http.StatusNotExtended,
-				http.StatusNetworkAuthenticationRequired:
-				ctx.String(code, http.StatusText(code))
-			default:
-				ctx.String(http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
-			}
-
-			return
-		},
+		handlerFunc:   simulateStatusCode,
+	},
+	{
+		httpMethod:    http.MethodPost,
+		relateivePath: "/_/status/:code",
+		handlerFunc:   simulateStatusCode,
 	},
 }
 
@@ -121,4 +48,87 @@ func loadPredefinedRoutes(router *gin.Engine) {
 			router.OPTIONS(route.relateivePath, route.handlerFunc)
 		}
 	}
+}
+
+func getStatusCodeAndText(code int) (int, string) {
+	switch code {
+	case http.StatusContinue,
+		http.StatusSwitchingProtocols,
+		http.StatusProcessing,
+		http.StatusEarlyHints,
+		http.StatusOK,
+		http.StatusCreated,
+		http.StatusAccepted,
+		http.StatusNonAuthoritativeInfo,
+		http.StatusNoContent,
+		http.StatusResetContent,
+		http.StatusPartialContent,
+		http.StatusMultiStatus,
+		http.StatusAlreadyReported,
+		http.StatusIMUsed,
+		http.StatusMultipleChoices,
+		http.StatusMovedPermanently,
+		http.StatusFound,
+		http.StatusSeeOther,
+		http.StatusNotModified,
+		http.StatusUseProxy,
+		http.StatusTemporaryRedirect,
+		http.StatusPermanentRedirect,
+		http.StatusBadRequest,
+		http.StatusUnauthorized,
+		http.StatusPaymentRequired,
+		http.StatusForbidden,
+		http.StatusNotFound,
+		http.StatusMethodNotAllowed,
+		http.StatusNotAcceptable,
+		http.StatusProxyAuthRequired,
+		http.StatusRequestTimeout,
+		http.StatusConflict,
+		http.StatusGone,
+		http.StatusLengthRequired,
+		http.StatusPreconditionFailed,
+		http.StatusRequestEntityTooLarge,
+		http.StatusRequestURITooLong,
+		http.StatusUnsupportedMediaType,
+		http.StatusRequestedRangeNotSatisfiable,
+		http.StatusExpectationFailed,
+		http.StatusTeapot,
+		http.StatusMisdirectedRequest,
+		http.StatusUnprocessableEntity,
+		http.StatusLocked,
+		http.StatusFailedDependency,
+		http.StatusTooEarly,
+		http.StatusUpgradeRequired,
+		http.StatusPreconditionRequired,
+		http.StatusTooManyRequests,
+		http.StatusRequestHeaderFieldsTooLarge,
+		http.StatusUnavailableForLegalReasons,
+		http.StatusInternalServerError,
+		http.StatusNotImplemented,
+		http.StatusBadGateway,
+		http.StatusServiceUnavailable,
+		http.StatusGatewayTimeout,
+		http.StatusHTTPVersionNotSupported,
+		http.StatusVariantAlsoNegotiates,
+		http.StatusInsufficientStorage,
+		http.StatusLoopDetected,
+		http.StatusNotExtended,
+		http.StatusNetworkAuthenticationRequired:
+		return code, http.StatusText(code)
+	}
+
+	return http.StatusBadRequest, http.StatusText(http.StatusBadRequest)
+}
+
+// simulateStatusCode generates responses based on the given status code.
+func simulateStatusCode(ctx *gin.Context) {
+	code, err := strconv.Atoi(ctx.Param("code"))
+	if err != nil {
+		ctx.String(http.StatusBadRequest, http.StatusText(http.StatusBadRequest))
+		return
+	}
+
+	statusCode, statusText := getStatusCodeAndText(code)
+	ctx.String(statusCode, statusText)
+	return
 }
