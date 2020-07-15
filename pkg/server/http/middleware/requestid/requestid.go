a2bef5df75d3ed8244fe95da1810362400b2276a
Agastyo Satriaji Idam <agastyo@ruangguru.com>
2020-07-15 20:27:04 +0700

    Inject request ID to HTTP response writer

--- pkg/server/http/middleware/requestid/requestid.go
+++ pkg/server/http/middleware/requestid/requestid.go
@@ -7,7 +7,7 @@ import (
 
 const HeaderXRequestID = "X-Request-ID"
 
-// New initializes the RequestID middleware.
+// New initializes the request ID middleware.
 func New() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		// Get id from request
@@ -18,6 +18,14 @@ func New() gin.HandlerFunc {
 			ctx.Request.Header.Set(HeaderXRequestID, rid)
 		}
 
+		// Attach the request ID to the response writer.
+		ctx.Header(HeaderXRequestID, rid)
+
 		ctx.Next()
 	}
+}
+
+// Get gets the request ID from the response writer.
+func Get(ctx *gin.Context) string {
+	return ctx.Writer.Header().Get(HeaderXRequestID)
 }
