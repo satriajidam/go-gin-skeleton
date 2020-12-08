cccec303176b0926e1dfddb2cc782cadc13b7ee9
Agastyo Satriaji Idam <agastyo@ruangguru.com>
2020-12-08 10:48:17 +0700

    Add function to record request size & count completed requests to metrics recorder

--- pkg/telemetry/http/http.go
+++ pkg/telemetry/http/http.go
@@ -1,5 +1,6 @@
-// Package http is based on https://github.com/slok/go-http-metrics
-// modified to use opentelemetry-go instead of prometheus go-client.
+// Package http is based on https://github.com/slok/go-http-metrics but
+// modified to use opentelemetry-go instead of prometheus go-client and
+// include some additional HTTP metrics to capture.
 package http
 
 import (
@@ -27,15 +28,18 @@ type HTTPProperties struct {
 	ID string
 }
 
-// Recorder knows how to record and measure the metrics. This
-// Interface has the required methods to be used with the HTTP
-// middlewares.
+// Recorder knows how to record and measure the metrics. This Interface has the required
+// methods to be used with the HTTP middlewares.
 type Recorder interface {
-	// ObserveHTTPRequestDuration measures the duration of an HTTP request.
-	ObserveHTTPRequestDuration(ctx context.Context, props HTTPReqProperties, duration time.Duration)
-	// ObserveHTTPResponseSize measures the size of an HTTP response in bytes.
-	ObserveHTTPResponseSize(ctx context.Context, props HTTPReqProperties, sizeBytes int64)
-	// AddInflightRequests increments and decrements the number of inflight request being processed.
+	// RecordHTTPRequestDuration measures the duration of an HTTP request.
+	RecordHTTPRequestDuration(ctx context.Context, props HTTPReqProperties, duration time.Duration)
+	// RecordHTTPRequestSize measures the size of an HTTP request in bytes.
+	RecordHTTPRequestSize(ctx context.Context, props HTTPReqProperties, sizeBytes int64)
+	// RecordHTTPResponseSize measures the size of an HTTP response in bytes.
+	RecordHTTPResponseSize(ctx context.Context, props HTTPReqProperties, sizeBytes int64)
+	// AddCompletedRequests increments the number of completed requests.
+	AddCompletedRequests(ctx context.Context, props HTTPProperties, quantity int)
+	// AddInflightRequests increments and decrements the number of inflight requests.
 	AddInflightRequests(ctx context.Context, props HTTPProperties, quantity int)
 }
 
@@ -44,8 +48,10 @@ const Dummy = dummy(0)
 
 type dummy int
 
-func (dummy) ObserveHTTPRequestDuration(_ context.Context, _ HTTPReqProperties, _ time.Duration) {}
-func (dummy) ObserveHTTPResponseSize(_ context.Context, _ HTTPReqProperties, _ int64)            {}
-func (dummy) AddInflightRequests(_ context.Context, _ HTTPProperties, _ int)                     {}
+func (dummy) RecordHTTPRequestDuration(_ context.Context, _ HTTPReqProperties, _ time.Duration) {}
+func (dummy) RecordHTTPRequestSize(_ context.Context, _ HTTPReqProperties, _ int64)             {}
+func (dummy) RecordHTTPResponseSize(_ context.Context, _ HTTPReqProperties, _ int64)            {}
+func (dummy) AddCompletedRequests(_ context.Context, _ HTTPProperties, _ int)                   {}
+func (dummy) AddInflightRequests(_ context.Context, _ HTTPProperties, _ int)                    {}
 
 var _ Recorder = Dummy
